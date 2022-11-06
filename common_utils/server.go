8143c715a689d6f6ea543099579d31dc7c5346eb
Siddhartha Raja <siddharth.raja@dunzo.in>
2022-11-06 17:50:51 +0530

    add server start script with graceful shutdown

---
+++ common_utils/server.go
@@ -0,0 +1,56 @@
+package commonutils
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+type Server struct {
+	r       *mux.Router
+	address string
+	srv     *http.Server
+}
+
+func NewServer() *Server {
+	return &Server{
+		r:       mux.NewRouter(),
+		address: fmt.Sprintf("%s:%s", viper.GetString("SERVER_HOST"), viper.GetString("SERVER_PORT")),
+	}
+}
+
+func (s *Server) ServeHttp() {
+	s.srv = &http.Server{
+		Handler: s.r,
+		Addr:    s.address,
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: time.Minute,
+		ReadTimeout:  time.Minute,
+	}
+
+	logrus.Info("Server starting at addr: ", s.address)
+	err := s.srv.ListenAndServe()
+
+	if err == http.ErrServerClosed {
+		logrus.Info("Server shut dowon")
+	} else if err != nil {
+		logrus.Error(err)
+	}
+
+}
+
+func (s *Server) Shutdown(goCtx context.Context) {
+	if s.srv == nil {
+		logrus.Error("Server not yet started.")
+		return
+	}
+
+	if err := s.srv.Shutdown(goCtx); err != nil {
+		logrus.Fatal("Server failed to shutdown: %v", err.Error())
+	}
+}
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	commonutils "github.com/siddharthrajaraja/web_server_template_go/common_utils"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	goCtx, cancel := context.WithCancel(context.Background())
+
+	initLogging()
+	SetupConfiguration()
+
+	defer func() {
+		cancel()
+	}()
+
+	server := commonutils.NewServer()
+
+	go func() {
+		server.ServeHttp()
+	}()
+
+	waitForShutdownSignal(server, goCtx, cancel)
+}
+
+func SetupConfiguration() {
+	viper.SetConfigFile(".env")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Panicf("Error while reading .env")
+	}
+}
+
+func waitForShutdownSignal(srv *commonutils.Server, goCtx context.Context, cancel context.CancelFunc) {
+	var gracefulStop = make(chan os.Signal)
+
+	signal.Notify(gracefulStop, syscall.SIGTERM)
+	signal.Notify(gracefulStop, syscall.SIGINT)
+	signal.Notify(gracefulStop, syscall.SIGQUIT)
+
+	select {
+	case <-gracefulStop:
+		cancel()
+		// if stop signal is received, wait for some time so that background workers get time to exit
+		<-time.After(5 * time.Second)
+	case <-goCtx.Done():
+		// shutdown if context was cancelled by something else before shutdown signal
+	}
+	srv.Shutdown(goCtx)
+}
+
+func initLogging() {
+	lvl, ok := os.LookupEnv("LOG_LEVEL")
+	if !ok {
+		lvl = "debug"
+	}
+	ll, err := logrus.ParseLevel(lvl)
+	if err != nil {
+		ll = logrus.DebugLevel
+	}
+	logrus.SetLevel(ll)
+}
